c8a584f1c7c9f6b1490d974a11810b352e7f3d44
“ijassm” <ijass.ocean@gmail.com>
2024-01-30 14:41:24 +0530

    -- 30 JAN 2024

--- functions.py
+++ functions.py
@@ -45,8 +45,70 @@ l5 = [15, 3, 45, 12, 7]
 # print(valueEqualToIndex(l5))
 
 
-def valueEqualToIndex(list, n):
-    return [list[i] for i in range(n) if list[i] == i + 1]
+# def valueEqualToIndex(list, n):
+#     return [list[i] for i in range(n) if list[i] == i + 1]
 
 
-print(valueEqualToIndex(l1, 5))
+# print(valueEqualToIndex(l1, 5))
+
+# Types of Arguments
+# Arguments are specified after the function name, inside the parentheses
+
+
+# def my_function(fname):
+#     print(fname + " Refsnes")
+
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+
+# Parameters or Arguments?
+
+# From a function's perspective:
+
+# A parameter is the variable listed inside the parentheses in the function definition.
+
+# An argument is the value that are sent to the function when it is called.
+
+# Arbitrary Arguments, *args
+
+
+# def sum(*values):
+#     print(values)
+#     s = 0
+#     for i in values:
+#         s += i
+
+#     return s
+
+
+# print(sum(2, 5))
+# print(sum(2, 5, 7))
+# print(sum(2, 5, 7, 2))
+
+
+# Keyword Arguments & Default Parameter Value
+
+
+# def my_function(name, age, location="pondy"):
+#     print("Name is", name)
+#     print("Age is", age)
+#     print("Location is", location)
+
+
+# my_function(location="chennai", name="xyz", age=10)
+# my_function(name="xyz", age=10)
+
+
+# Arbitrary Keyword Arguments, **kwargs
+
+
+# def my_function(**person):
+#     print("Name is", person["name"])
+#     print("Age is", person["age"])
+#     print("Location is", person["location"])
+
+
+# my_function(location="chennai", name="xyz", age=10)
